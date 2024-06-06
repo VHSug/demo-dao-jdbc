2cb0f009515e02fb58828f823b452c560502b4a1
VHSug <vitorhidekisuga@hotmail.com>
2024-06-06 15:08:04 -0300

    DEPARTAMENTO ADICIONAR TODOS

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -3,6 +3,9 @@ package application;
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
 
 public class Program2 {
     public static void P2(){
@@ -12,5 +15,12 @@ public class Program2 {
         Department department = departmentDao.findById(1);
         System.out.println(department);
 
+        System.out.println();
+
+        System.out.println("==== ENCONTRAR TODOS ====");
+        List<Department> list = departmentDao.findAll();
+        for (Department obj : list) {
+            System.out.println(obj);
+        }
     }
 }
