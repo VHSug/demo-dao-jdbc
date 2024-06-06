0744152b5d2a8578bc6cf6e16b813b969c301ed9
unknown <vitorhidekisuga@hotmail.com>
2024-06-06 09:04:33 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void Prog(){
 
         Department obj = new Department(1, "Boks");
-        System.out.println(obj);
+        Seller seller = new Seller(obj, 3000.00, new Date(),"bob@gmail.com", "Bob", 21);
+        System.out.println(seller);
     }
 }
