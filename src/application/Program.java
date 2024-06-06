f3423afb3f424933b46e5aabb920ca78bc97d231
unknown <vitorhidekisuga@hotmail.com>
2024-06-06 08:49:03 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+    public static void Prog(){
+
+        Department obj = new Department(1, "Boks");
+        System.out.println(obj);
+    }
+}
