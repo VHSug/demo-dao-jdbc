a82587e5eb57b6d76e1175f59b352e4d251a5328
VHSug <vitorhidekisuga@hotmail.com>
2024-06-06 10:26:38 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,8 +11,8 @@ public class Program {
     public static void Prog(){
 
         Department obj = new Department(1, "Boks");
-        Seller seller = new Seller(obj, 3000.00, new Date(),"bob@gmail.com", "Bob", 21);
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
